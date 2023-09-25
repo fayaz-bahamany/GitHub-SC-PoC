6a63dd87123d77d1c22c2ac4ec30556081b7ac65
fayaz-bahamany <140581005+fayaz-bahamany@users.noreply.github.com>
2023-09-25 16:14:09 +0400

    Initial commit

---
+++ src/main/java/com/github/poc/java_project/GetResult.java
@@ -0,0 +1,36 @@
+
+public class GetResult  
+{  
+public static int add(int i, int j)  
+{ 
+
+int sum = 0;
+sum = i + j;
+    System.out.println("The sum of the numbers is: "+sum);  
+
+return sum;
+  
+}  
+
+public static int subtract(int i, int j)  
+{ 
+
+int diff = 0;
+diff = i - j;
+    System.out.println("The difference of the numbers is: "+diff);  
+
+return diff;
+  
+} 
+
+public static int multiply(int i, int j)  
+{ 
+
+int result = 0;
+result = i * j;
+    System.out.println("The result of the numbers is: "+result);  
+
+return result;
+  
+} 
+}  
